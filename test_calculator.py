2b3b6c17645b366de6d3672bd055bd5dca0ed52c
Freddy K <89672133+Fadykolta@users.noreply.github.com>
2023-03-19 23:11:12 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,21 @@
-import unittest
-from calculator import Calculator
+import calculator
 
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        calc = Calculator()
-        self.assertEqual(calc.add(2, 3), 5)
-        self.assertEqual(calc.add(-2, 3), 1)
-    
-    def test_subtract(self):
-        calc = Calculator()
-        self.assertEqual(calc.subtract(2, 3), -1)
-        self.assertEqual(calc.subtract(-2, 3), -5)
+def test_add():
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
+    assert calculator.add(-2, -2) == -4
 
-if __name__ == '__main__':
-    unittest.main()
+def test_subtract():
+    assert calculator.subtract(2, 3) == -1
+    assert calculator.subtract(-1, 1) == -2
+    assert calculator.subtract(-2, -2) == 0
+
+def test_multiply():
+    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(-1, 1) == -1
+    assert calculator.multiply(-2, -2) == 4
+
+def test_divide():
+    assert calculator.divide(6, 3) == 2
+    assert calculator.divide(-6, 3) == -2
+    assert calculator.divide(3, 0) == ValueError("Cannot divide by zero")
